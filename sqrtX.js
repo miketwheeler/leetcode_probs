44e98db1eeed12f2e7469ed99ddcca98a61caa2f
miketwheeler <mikeytwheeler@gmail.com>
2022-07-15 23:30:19 -0700

    another one

--- sqrtX.js
+++ sqrtX.js
@@ -8,27 +8,23 @@
 const in1 = 4
 const in2 = 8
 
+// compares no-square situations and early returns else iterate until a squared int > 1 just passes 'x', then if it just passes 'x' -> subtract 1 
+//          from the square to 'round down' to the next lower square to return
 
 function mySqrt(x) {
-    return Math.sqrt(x).
+    let sq = 1;
+    
+    if(x===0 || x===1)
+        return x;
+    
+    while(x > sq*sq) 
+        sq++;
+    
+    if(x < sq*sq)
+        sq--;
+    
+    return sq;
 };
 
 
 console.log(`returned: ${mySqrt(in1)}`)
-
-
-// var mySqrt = function(x) {
-//     if (x === 0 || x === 1) return x;
-//     let start = 1;
-//     let end = Math.floor(x / 2);
-//     while (start <= end) {
-//       const mid = Math.floor((end - start) / 2 + start);
-//       const v1 = mid ** 2;
-//       const v2 = (mid + 1) ** 2;
-//       if (v1 === x || (v1 < x && v2 > x)) return mid;
-//       if (v1 < x) start = mid + 1;
-//       else if (v1 > x) end = mid - 1;
-//     }
-    
-//     return null;
-//   };
